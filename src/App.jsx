7374b9077dc7b767235f8396859440cbade09220
driftmeisterbj <ak.stra88@gmail.com>
2025-12-30 16:29:06 +0100

    Satt opp React

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { IoLogoGithub } from "react-icons/io5"
+import { MdMailOutline } from "react-icons/md";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <h1>bjarneboi</h1>
+        <p className="about-me">
+          Test
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="linksContainer">
+        <a href="https://github.com/bjarneboi">
+          <IoLogoGithub className="logo" alt="GitHub logo" size={96} />
+        </a>
+        <a href="mailto:arneks@hotmail.com">
+          <MdMailOutline className="logo" alt="Email icon" size={96} />
+        </a>
+      </div>
     </>
   )
 }
