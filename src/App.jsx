e588ee86c0329be3ce62ba0f60ec2e8e239f5ed4
driftmeisterbj <ak.stra88@gmail.com>
2026-01-02 18:17:15 +0100

    Made more semantic + converted to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
 import { IoLogoGithub } from "react-icons/io5"
 import { MdMailOutline } from "react-icons/md";
 import { FaLinkedin } from "react-icons/fa";
-import './App.css'
+import './App.scss'
 import { LinkBox } from "./components/LinkBox";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>bjarneboi</h1>
-        <div className="about-me">
-          <span>Hi! My name is Arne, I am a 27 year old Computer Science student from Norway.</span>
-          <span>I enjoy developing software, in particular mobile applications.</span>
-          <span>For business inquiries and other contact, you can reach me through the links below. :)</span>
+    <div className="main-container">
+      <h1>bjarneboi</h1>
+      <section className="about-me">
+        <p className="am-p">Hi! My name is Arne, I am a 27 year old Computer Science student from Norway.</p>
+        <p className="am-p">I love software development, with my main passion being mobile applications.</p>
+      </section>
+      <section className="links">
+        <p className="am-p">For business inquiries and other contact, you can reach me through the links below. :)</p>
+        <div className="linkbox-container">
+          <LinkBox 
+            icon={<IoLogoGithub className="logo" aria-label="Github logo" size={96} />}
+            text="Github"
+            url="https://github.com/bjarneboi"
+          />
+          <LinkBox 
+            icon={<MdMailOutline className="logo" aria-label="Email icon" size={96} />}
+            text="Email"
+            url="mailto:arneks@hotmail.com" 
+          />
+          <LinkBox 
+            icon={<FaLinkedin className="logo" aria-label="LinkedIn logo" size={96} />}
+            text="LinkedIn"
+            url="https://www.linkedin.com/in/arne-krogdahl-stramme/"
+          />
         </div>
-      </div>
-
-      <div className="linksContainer">
-        <LinkBox 
-          icon={<IoLogoGithub className="logo" alt="Github logo" size={96} />}
-          text="Github"
-          url="https://github.com/bjarneboi"
-        />
-        <LinkBox 
-          icon={<MdMailOutline className="logo" alt="Email icon" size={96} />}
-          text="Email"
-          url="mailto:arneks@hotmail.com" 
-        />
-        <LinkBox 
-          icon={<FaLinkedin className="logo" alt="LinkedIn logo" size={96} />}
-          text="LinkedIn"
-          url="https://www.linkedin.com/in/arne-krogdahl-stramme/"
-        />
-      </div>
-    </>
+      </section>
+    </div>
   )
 }
 
