a88715469eabffe8ef8c38b9aacfe5a95ea9fc0a
driftmeisterbj <ak.stra88@gmail.com>
2026-01-02 18:56:34 +0100

    Made the website scale with size

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,24 @@ function App() {
     <div className="main-container">
       <h1>bjarneboi</h1>
       <section className="about-me">
-        <p className="am-p">Hi! My name is Arne, I am a 27 year old Computer Science student from Norway.</p>
+        <p className="am-p">Hi! My name is Arne and I'm a 27 year old Computer Science student from Norway.</p>
         <p className="am-p">I love software development, with my main passion being mobile applications.</p>
       </section>
       <section className="links">
         <p className="am-p">For business inquiries and other contact, you can reach me through the links below. :)</p>
         <div className="linkbox-container">
           <LinkBox 
-            icon={<IoLogoGithub className="logo" aria-label="Github logo" size={96} />}
+            icon={<IoLogoGithub className="logo" aria-label="Github logo" />}
             text="Github"
             url="https://github.com/bjarneboi"
           />
           <LinkBox 
-            icon={<MdMailOutline className="logo" aria-label="Email icon" size={96} />}
+            icon={<MdMailOutline className="logo" aria-label="Email icon" />}
             text="Email"
             url="mailto:arneks@hotmail.com" 
           />
           <LinkBox 
-            icon={<FaLinkedin className="logo" aria-label="LinkedIn logo" size={96} />}
+            icon={<FaLinkedin className="logo" aria-label="LinkedIn logo" />}
             text="LinkedIn"
             url="https://www.linkedin.com/in/arne-krogdahl-stramme/"
           />
